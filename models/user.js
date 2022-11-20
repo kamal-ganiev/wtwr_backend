14431d0d8ec8dbc878d6369715f8f69f2f29ba8f
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-11-20 10:01:04 -0500

    Expand User Schema

--- models/user.js
+++ models/user.js
@@ -4,19 +4,34 @@ const validator = require('validator');
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Elise Bauer',
     maxlength: 30,
     minlength: 2,
   },
   avatar: {
     type: String,
-    required: true,
+    default:
+      'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png',
     validate: {
       validator(value) {
         return validator.isURL(value);
       },
       message: 'You must enter a valid URL',
     },
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: 'You must enter a valid email address',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
   },
 });
 
