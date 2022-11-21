8b2e808f4a09d466172943682deb7d76c9666082
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-11-20 23:02:50 -0500

    Delete hardcored user

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ runServer('mongodb://localhost:27017/wtwr_db');
 app.use(helmet());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '636fe3aca99c6f50a259b8ef',
-  };
-
-  next();
-});
-
 app.use('/items', require('./routes/clothingItems'));
 app.use('/users', require('./routes/users'));
 app.use('*', require('./routes/errorHandler'));
