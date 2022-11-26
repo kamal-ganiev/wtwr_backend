6260179a45934797d66216524f6e92cb5f2fc1a5
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-11-26 02:47:49 -0500

    Fix minor bugs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 const helmet = require('helmet');
-const cors = require("cors");
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const { runServer } = require('./utils/config');
 const { PORT } = require('./utils/constants');
-require('dotenv').config();
 
 const app = express();
 runServer('mongodb://localhost:27017/wtwr_db');
@@ -17,6 +17,7 @@ app.use('/signin', login);
 app.use('/items', require('./routes/clothingItems'));
 app.use('/users', require('./routes/users'));
 app.use('*', require('./routes/errorHandler'));
+
 app.use(cors());
 
 app.listen(PORT);
